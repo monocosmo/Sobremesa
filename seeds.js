66f701368e2f54ed918d9cb5ede70841cc19c527
monocosmo <shuchiwu@gmail.com>
2018-08-17 14:05:35 +0000

    change seed DB data content

--- seeds.js
+++ seeds.js
@@ -1,52 +1,41 @@
 var mongoose   = require("mongoose");
-var Campground = require("./models/campground");
-var Comment    = require("./models/comment");
+var Blog = require("./models/blog");
 
 var data = [
     {
-        name: "Fox1",
+        title: "Fox1",
         image: "https://images.freeimages.com/images/small-previews/72c/fox-1522156.jpg",
-        description: "This is a red fox1."
+        body: "This is a red fox1.",
+        created: {type: Date, default: Date.now}
     },
     {
         name: "Fox2",
         image: "https://images.freeimages.com/images/small-previews/72c/fox-1522156.jpg",
-        description: "This is a red fox2."
+        body: "This is a red fox2.",
+        created: {type: Date, default: Date.now}
     },
     {
         name: "Fox3",
         image: "https://images.freeimages.com/images/small-previews/72c/fox-1522156.jpg",
-        description: "This is a red fox3."
+        body: "This is a red fox3.",
+        created: {type: Date, default: Date.now}
     }
 ];
 
 function seedDB(){
-    // Remove all campgrounds
-    Campground.remove({}, function(err){
+    // Remove all data
+   Blog.remove({}, function(err){
         if(err){
             console.log(err);
         }
-        console.log("removed campgrounds");
-        // add a few campgrounds
+        console.log("removed content");
+        // add seed data
         data.forEach(function(seed){
-            Campground.create(seed, function(err, campground){
+            Blog.create(seed, function(err, blog){
                 if(err){
                     console.log(err);
                 }else{
-                    console.log("added a campground");
-                    // create a comment
-                    Comment.create({
-                        text: "This fox is red.",
-                        author: "Fox Red"
-                    }, function(err, comment){
-                        if(err){
-                            console.log(err);
-                        }else{
-                            campground.comments.push(comment);
-                            campground.save();
-                            console.log("Created a new comment");
-                        }
-                    });
+                    console.log("added a blog");
                 }
             });
         });
